82c53f04f1505ae862f063530c4b5085dd4d1c63
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2026-04-10 11:05:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ YT_API_BASE: str = os.getenv("YT_API_BASE", "https://yt.teamdev.sbs/api/v1/")
 
 TEAM_NAME: str = os.getenv("TEAM_NAME", "TeamDev")
 DEVELOPER_NAME: str = os.getenv("DEVELOPER_NAME", "@MR_ARMAN_08")
-BOT_USERNAME: str = os.getenv("BOT_USERNAME", "@Ktktjtktkykybkobot")
+BOT_USERNAME: str = os.getenv("BOT_USERNAME", "@Daily2tele_bot")
 
 DAILY_DOWNLOAD_LIMIT: int = int(os.getenv("DAILY_DOWNLOAD_LIMIT", "3"))
 
 FORCE_JOIN_CHANNEL: str = os.getenv("FORCE_JOIN_CHANNEL", "https://t.me/CrimeZone_Update")
-FORCE_JOIN_ENABLED: bool = os.getenv("FORCE_JOIN_ENABLED", "true").lower() == "true"
+FORCE_JOIN_ENABLED: bool = os.getenv("FORCE_JOIN_ENABLED", "false").lower() == "true"
 
 REFERRAL_DOWNLOADS_PER_REF: int = int(os.getenv("REFERRAL_DOWNLOADS_PER_REF", "1"))
 
@@ -30,5 +30,5 @@ _FAKE_SEGMENT = "Jejdjdidjdjhidden_apjjsjdjskdi"
 TERABOX_API_KEY: str = os.getenv("TERABOX_API_KEY", "teamdev_jirvspco3y")
 TERABOX_API_BASE: str = os.getenv("TERABOX_API_BASE", "https://api.teamdev.sbs/v2/download")
 
-MONGO_URI: str = os.getenv("MONGO_URI", "mongodb+srv://teamxxxxxx@cluster0.6xywr7u.mongodb.net/?appName=Cluster0")
+MONGO_URI: str = os.getenv("MONGO_URI", "mongodb+srv://ENCODE:ENCODE@cluster0.gkibxvp.mongodb.net/?appName=Cluster0")
 MONGO_DB:  str = os.getenv("MONGO_DB",  "teamdev_downbot") # Make @YourTeam_DownBot 👌
